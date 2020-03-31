287227157b22c99336925d5ce47bb720f755fd71
Michael Berliner <62532336+michaelberliner@users.noreply.github.com>
2020-03-31 15:24:50 -0400

    Update sketch.js

--- Photography/sketch.js
+++ Photography/sketch.js
@@ -1,6 +1,6 @@
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight*10.25);
+  canvas = createCanvas(windowWidth, windowHeight*9);
  canvas.position(0,0);
     canvas.style('z-index','-1');
     
@@ -12,6 +12,6 @@ function draw() {
    for (let i=1300; i<2000; i++){
      let i
   fill(random(i), random(255), random(255), random(100));
-    ellipse(random(windowWidth), random(windowHeight*10.25), random(i));
+    ellipse(random(windowWidth), random(windowHeight*9), random(i));
 }
 }
