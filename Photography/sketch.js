caef481ea611114a11d5bf186dc90050203f20bd
Michael Berliner <62532336+michaelberliner@users.noreply.github.com>
2020-03-31 16:10:06 -0400

    photo

--- Photography/sketch.js
+++ Photography/sketch.js
@@ -1,17 +1,21 @@
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
+
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight*9);
- canvas.position(0,0);
+    canvas.position(0,0);
     canvas.style('z-index','-1');
-    
+  
   }
 
 
 function draw() {
   
-   for (let i=1300; i<2000; i++){
+   for (let i=100; i<200; i++){
      let i
-  fill(random(i), random(255), random(255), random(100));
+  fill(random(i));
     ellipse(random(windowWidth), random(windowHeight*9), random(i));
 }
 }
