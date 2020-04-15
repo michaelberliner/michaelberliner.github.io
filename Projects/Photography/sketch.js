f114bba230c91c4bea506a4a164089e54623c02c
Michael Berliner <62532336+michaelberliner@users.noreply.github.com>
2020-04-15 18:48:11 -0400

    StyleUpdates

---
+++ Projects/Photography/sketch.js
@@ -0,0 +1,19 @@
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+   
+}
+function setup() {
+  canvas = createCanvas(windowWidth,windowHeight);
+    canvas.style('z-index','-1');
+    
+  }
+
+
+function draw() {
+   for (let i=0; i<160; i++){
+     let i
+  fill(random(i));
+    ellipse(random(windowWidth), random(windowHeight), random(i));
+}
+}
