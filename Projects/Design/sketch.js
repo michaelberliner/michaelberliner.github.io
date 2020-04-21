d43d0950d62effc9a6879d5bf893fe58947f8d95
Michael Berliner <62532336+michaelberliner@users.noreply.github.com>
2020-04-21 01:23:45 -0400

    morepages

---
+++ Projects/Design/sketch.js
@@ -0,0 +1,20 @@
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+ canvas.position(0,0);
+    canvas.style('z-index','-1');
+    
+  }
+
+
+function draw() {
+
+   for (let i=0; i<60; i++){
+     let s=map(100,30,0,500,50);
+     frameRate(5);
+  fill(random(255), random(i), random(10), random(100));
+    ellipse(random(windowWidth), random(windowHeight), random(s));
+}
+}
